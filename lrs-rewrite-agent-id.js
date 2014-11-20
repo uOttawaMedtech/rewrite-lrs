16690d9c8d2808c2d65260d9522b41f3d308e383
Pascal Laliberté <plaliberte@uOttawa.ca>
2014-11-20 14:45:16 -0500

    Tested retrieveState

--- lrs-rewrite-agent-id.js
+++ lrs-rewrite-agent-id.js
@@ -1,3 +1,20 @@
 var TinCan = require('tincanjs');
-
 var Config = require('./config.js');
+
+var lrs = new TinCan.LRS(Config.record_store);
+
+list_keys = function (err, result) {
+  if (err === null) {
+    console.log(result);
+  } else {
+    console.log(result);
+  }
+};
+
+var state_keys = lrs.retrieveState(null,
+  {
+    activity: new TinCan.Activity({ id : Config.activity_id }),
+    agent: new TinCan.Agent({ mbox: Config.agent_mbox }),
+    callback: list_keys
+  }
+);
