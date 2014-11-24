e7f8121926f17bba8523c062c81b19418184cc5e
Pascal Laliberté <plaliberte@uOttawa.ca>
2014-11-24 14:34:42 -0500

    Tested saving, deleting and verifying state values

--- lrs-rewrite-agent-id.js
+++ lrs-rewrite-agent-id.js
@@ -3,7 +3,8 @@ var Config = require('./config.js');
 
 var lrs = new TinCan.LRS(Config.record_store);
 
-list_keys = function (err, result) {
+output_result = function (err, result) {
+  console.log( "\n\nretrieveState\n\n" );
   if (err === null) {
     console.log(result);
   } else {
@@ -16,6 +17,72 @@ var result = lrs.retrieveState(Config.stateId,
     activity: new TinCan.Activity({ id : Config.activity_id }),
     agent: new TinCan.Agent({ mbox: Config.agent_mbox }),
     registration: Config.registration,
-    callback: list_keys
+    callback: output_result
   }
 );
+
+test_save = function () {
+  var result = lrs.saveState('test-save-delete', Date.now(),
+    {
+      activity: new TinCan.Activity({ id : Config.activity_id }),
+      agent: new TinCan.Agent({ mbox: Config.agent_mbox }),
+      registration: Config.registration,
+      callback: test_retrieve_after_save
+    }
+  );
+}
+
+test_retrieve_after_save = function(err, result) {
+  var result = lrs.retrieveState(null,
+    {
+      activity: new TinCan.Activity({ id : Config.activity_id }),
+      agent: new TinCan.Agent({ mbox: Config.agent_mbox }),
+      registration: Config.registration,
+      callback: test_retrieve_after_save_output
+    }
+  );
+}
+
+test_retrieve_after_save_output = function (err, result) {
+  console.log( "\n\nsaveState test-save-delete\n\n" );
+  if (err === null) {
+    console.log(result);
+  } else {
+    console.log(result);
+  }
+
+  test_delete();
+};
+
+test_delete = function() {
+  var result = lrs.dropState('test-save-delete',
+    {
+      activity: new TinCan.Activity({ id : Config.activity_id }),
+      agent: new TinCan.Agent({ mbox: Config.agent_mbox }),
+      registration: Config.registration,
+      callback: test_retrieve_after_delete
+    }
+  );
+}
+
+test_retrieve_after_delete = function(err, result) {
+  var result = lrs.retrieveState(null,
+    {
+      activity: new TinCan.Activity({ id : Config.activity_id }),
+      agent: new TinCan.Agent({ mbox: Config.agent_mbox }),
+      registration: Config.registration,
+      callback: test_retrieve_after_delete_output
+    }
+  );
+}
+
+test_retrieve_after_delete_output = function (err, result) {
+  console.log( "\n\ndropState test-save-delete\n\n" );
+  if (err === null) {
+    console.log(result);
+  } else {
+    console.log(result);
+  }
+};
+
+test_save();
