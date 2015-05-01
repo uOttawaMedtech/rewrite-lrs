ad4b3401749ba0019a89ef99fba15ee4c0f0d7d2
Pascal Laliberté <plaliberte@uOttawa.ca>
2015-05-01 16:36:29 -0400

    Fetch first set of statements

--- lrs-rewrite-agent-id.js
+++ lrs-rewrite-agent-id.js
@@ -8,10 +8,14 @@ var lrs = new TinCan.LRS(Config.record_store);
 var rw = new LRSRewriter(lrs);
 
 rw.replaceAgent(
-    {
-        agent: new TinCan.Agent({ mbox: Config.agent_mbox }),
-    },
-    {
-        agent: new TinCan.Agent({ mbox: Config.new_agent_mbox }),
+    new TinCan.Agent({ mbox: Config.agent_mbox }),
+    new TinCan.Agent({ mbox: Config.new_agent_mbox }),
+    function(err, success) {
+        if (err)
+            return console.log("An error has occured")
+
+        if (success)
+        console.log("Done fetching agents for the following statements")
+
     }
 );
