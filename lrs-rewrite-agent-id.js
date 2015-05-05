32a6638855ab96995f6de386241fb6375f81c00e
Pascal Laliberté <plaliberte@uOttawa.ca>
2015-05-05 16:49:51 -0400

    Add saveStatement tasks to tasks.db

--- lrs-rewrite-agent-id.js
+++ lrs-rewrite-agent-id.js
@@ -5,19 +5,18 @@ var Config = require('./config.js');
 LRSRewriter.DEBUG = true;
 
 var lrs = new TinCan.LRS(Config.record_store);
-var rw = new LRSRewriter(lrs, function(err, rw) {
-    
+new LRSRewriter(lrs, function(err, rw) {
+    console.log('Done initiating the database')
+    rw.replaceAgent(
+        new TinCan.Agent({ mbox: Config.agent_mbox }),
+        new TinCan.Agent({ mbox: Config.new_agent_mbox }),
+        function(err, success) {
+            if (err)
+                return console.log("An error has occured")
+
+            if (success)
+            console.log("Done fetching agents for the following statements")
+
+        }
+    );
 });
-
-rw.replaceAgent(
-    new TinCan.Agent({ mbox: Config.agent_mbox }),
-    new TinCan.Agent({ mbox: Config.new_agent_mbox }),
-    function(err, success) {
-        if (err)
-            return console.log("An error has occured")
-
-        if (success)
-        console.log("Done fetching agents for the following statements")
-
-    }
-);
