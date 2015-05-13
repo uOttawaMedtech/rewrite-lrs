7673b6b8d806ab57451727cfc2465482c484d0f9
Pascal Laliberté <plaliberte@uOttawa.ca>
2015-05-13 15:12:51 -0400

    Execute queryStatements and moreStatements tasks

--- lrs-rewrite-agent-id.js
+++ lrs-rewrite-agent-id.js
@@ -8,15 +8,15 @@ var lrs = new TinCan.LRS(Config.record_store);
 new LRSRewriter(lrs, function(err, rw) {
     console.log('Done initiating the database')
     rw.dryrun = true
-    rw.replaceAgent(
+    rw.initReplaceAgent(
         new TinCan.Agent({ mbox: Config.agent_mbox }),
         new TinCan.Agent({ mbox: Config.new_agent_mbox }),
-        function(err, success) {
+        function(err, stepstaken) {
             if (err)
-                return console.log("An error has occured")
+                return console.log("An error has occured: " + err)
 
-            if (success)
-            console.log("Done fetching agents for the following statements")
+            console.log(stepstaken.message)
+
             rw.startTasks(function(err, numTasksCompleted, numTasksQueued){
                 console.log("finished " + numTasksCompleted + " tasks of " + numTasksQueued + " queued")
             })
