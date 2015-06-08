783a30de537e530f7f6b5901ea400c71d87b0054
Pascal Laliberté <plaliberte@uOttawa.ca>
2015-06-08 11:49:14 -0400

    Keep original agent sub-properties when re-issuing new statement or state entries

--- lrs-rewrite-agent-id.js
+++ lrs-rewrite-agent-id.js
@@ -9,8 +9,8 @@ new LRSRewriter(lrs, new TinCan.Agent(Config.issuer), function(err, rw) {
     console.log('Done initiating the database')
     rw.dryrun = true
     rw.replaceAgent(
-        new TinCan.Agent(Config.old_agent),
-        new TinCan.Agent(Config.new_agent),
+        Config.old_agent,
+        Config.new_agent,
         function(err, stepstaken) {
             if (err)
                 return console.log("An error has occured: " + err)
